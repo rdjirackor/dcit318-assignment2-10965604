89ff25b224971ab5b1301e77245ce4e565db8ae0
RichardDjirackor <rdjirackor@st.ug.edu.gh>
2024-06-17 14:05:20 +0000

    Completed Inheritance and Method Overriding

--- InheritanceAndMethodOverriding/InheritanceAndMethodOverriding/Program.cs
+++ InheritanceAndMethodOverriding/InheritanceAndMethodOverriding/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace AnimalSounds
+{
+    class Animal
+    {
+       public  virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+    class Dog : Animal
+    {
+public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }    
+    }
+    class Cat : Animal
+    {
+ public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program()
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
