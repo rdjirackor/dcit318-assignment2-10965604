fdaa2f4c0230c5124b5960c6076fa63aa425c828
RichardDjirackor <rdjirackor@st.ug.edu.gh>
2024-06-17 14:35:27 +0000

    Completed  Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Movables
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            car.Move();         
+            bicycle.Move();     
+        }
+    }
+}
